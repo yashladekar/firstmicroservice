11e9dd0c68ccdd47e2873a16e858e3795aa9242e
yash ladekar <yashladekar002@gamil.com>
2026-01-03 04:56:03 +0530

    feat(sap-core-service): initialize sap-core-service with Express, Prisma, and essential middleware

---
+++ apps/sap-core-service/src/config/env.ts
@@ -0,0 +1,25 @@
+import { z } from "zod"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    PORT: z.string().default("8085"),
+    LOG_LEVEL: z.enum(["debug", "info", "warn", "error"]).default("info"),
+    DATABASE_URL: z.url(),
+    REDIS_URL: z.url(),
+    FILE_STORAGE_PATH: z.string().default("/data/files"),
+    JWT_SECRET: z.string(),
+})
+const _env = envSchema.parse(process.env);
+
+export const env = {
+    NODE_ENV: _env.NODE_ENV,
+    PORT: parseInt(_env.PORT, 10),
+    LOG_LEVEL: _env.LOG_LEVEL,
+    DATABASE_URL: _env.DATABASE_URL,
+    REDIS_URL: _env.REDIS_URL,
+    FILE_STORAGE_PATH: _env.FILE_STORAGE_PATH,
+    JWT_SECRET: _env.JWT_SECRET,
+}
---
+++ apps/sap-core-service/src/server.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv"
+dotenv.config()
+import fs from "fs"
+import app from "./app"
+import { logger } from "./utils/logger"
+
+
+const uploadDir = "uploads/notes";
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+
+const PORT = process.env.PORT || 8085
+
+const server = app.listen(PORT, () => {
+    logger.info(`Server is running on port ${PORT} as ${process.env.NODE_ENV}`);
+});
+
+const gracefulShutdown = () => {
+    logger.info('Received kill signal, shutting down gracefully');
+    server.close(() => {
+        logger.info('Closed out remaining connections');
+        process.exit(0);
+    });
+
+    // Force close if it takes too long (e.g., 10 seconds)
+    setTimeout(() => {
+        logger.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 10000);
+};
+
+// Listen for termination signals
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
